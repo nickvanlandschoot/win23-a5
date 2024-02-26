3577ed85fc79800a2d903ffc0b3521b2bc7fd589
Nicholas Van Landschoot <nicholas.vanlandschoot@synthine.ai>
2024-02-26 00:22:33 -0700

    done

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# (The absolute path to the program is provided as the first and only argument.)
 CALCULATOR=$1
 
 echo "We've set up a GitHub Actions Workflow that will run all"
@@ -11,21 +10,41 @@ echo "To end a test early as a success, you should use the exit 0 command."
 
 echo "Invoke your program with the \$CALCULATOR variable; an example is below"
 
-
-# Test 01: Ensure the program runs without error with a simple, valid invocation.
-if ! $CALCULATOR 1 + 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+if ! $CALCULATOR 1 + 1; then
   echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
   exit 1
 fi
 
-# Test 02: Ensure simple case has correct output...
-if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then  # If the output of the program is not 2...
+if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then
   echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
   exit 1
 fi
 
-# Test 03: Ensure program errors with an invalid operand
-if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. success)...
+if $CALCULATOR 3 @ 2; then
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
+  exit 1
+fi
+
+if ! $CALCULATOR 5 - 3; then
+  exit 1
+fi
+
+if [[ $($CALCULATOR 5 - 3) -ne 2 ]]; then
+  exit 1
+fi
+
+if $CALCULATOR 1 / 0; then
+  exit 1
+fi
+
+if ! $CALCULATOR 10 * 2; then
+  exit 1
+fi
+
+if [[ $($CALCULATOR 10 * 2) -ne 20 ]]; then
+  exit 1
+fi
+
+if [[ $($CALCULATOR 9 / 3) -ne 3 ]]; then
   exit 1
 fi
